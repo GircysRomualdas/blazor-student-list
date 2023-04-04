ecdf3e4fc185148d92ce4877d3b6b9ad7dd28c32
Rudis159 <77154023+Rudis159@users.noreply.github.com>
2023-04-04 10:44:18 +0300

    Add models

---
+++ BlazorCrud/BlazorCrud/Shared/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace BlazorCrud.Shared
+{
+    internal class Student
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public StudentGroup? StudentGroup { get; set; }
+        public int StudentId { get; set; }
+    }
+}
